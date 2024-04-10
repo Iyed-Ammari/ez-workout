649f636592a993e740612cddc701b2722060f447
Iyed Ammari <ammariiyed@isamm.u-manouba.tn>
2024-04-10 06:00:21 +0100

    Refactor ExerciseCard, Exercises, ExerciseDetail, and fetchData files

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,16 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Stack, Typography } from '@mui/material';
 
-const ExerciseCard = ({ exercise }) => {
-  return exercise.name === "not found" ? (
-    <Box >
-      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy"  />
-    </Box>
-  ) : (
-    <Link to={`/exercise/${exercise.id}`} className="exercise-card">
-      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy"     />
-      <Stack direction="row">
+const ExerciseCard = ({ exercise }) => (
+  <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+    <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+    <Stack direction="row">
       <Button sx={{ ml: '21px', color: '#fff', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
         {exercise.bodyPart}
       </Button>
@@ -21,8 +16,7 @@ const ExerciseCard = ({ exercise }) => {
     <Typography ml="21px" color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
       {exercise.name}
     </Typography>
-    </Link>
-  );
-};
+  </Link>
+);
 
 export default ExerciseCard;
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
 import Loader from '../components/Loader';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import exercisesData from '../data/dummyData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
+import Navbar from '../components/Navbar';
 
 const ExerciseDetail = () => {
   const [loading, setLoading] = React.useState(false);
@@ -9,9 +17,22 @@ const ExerciseDetail = () => {
       setLoading(false)
     }, 3000)
   }, []);
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const exerciseDetailData = exercisesData.find(exercise => exercise.id === id);
+    setExerciseDetail(exerciseDetailData);
+  }, [id]);
+
   return loading? <Loader loading={loading} /> : (
-    <div>ExerciseDetail</div>
-  )
+    <Box>
+      <Navbar />
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
 }
 
 export default ExerciseDetail
